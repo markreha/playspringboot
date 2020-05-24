f09a3b4b02e3957ea0ea928d3234922bdaace0a9
Mark Reha <markreha@mac.com>
2020-05-23 18:46:14 -0700

    Update PlayspringbootApplication.java

--- src/main/java/com/gcu/PlayspringbootApplication.java
+++ src/main/java/com/gcu/PlayspringbootApplication.java
@@ -3,12 +3,14 @@ package com.gcu;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
+
 @EnableEncryptableProperties
 @SpringBootApplication
-public class PlayspringbootApplication {
-
-	public static void main(String[] args) {
+public class PlayspringbootApplication 
+{
+	public static void main(String[] args) 
+	{
 		SpringApplication.run(PlayspringbootApplication.class, args);
 	}
-
 }
